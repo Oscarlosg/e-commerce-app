02f4a85e8bf9bc5d89d0036e19ba3c8e429931f6
oscarlos Gomez Rosario <oscarlosg14@hotmail.com>
2023-11-17 20:08:22 -0500

    adding google authorization.

--- e-commerce-admin/pages/index.js
+++ e-commerce-admin/pages/index.js
@@ -1,5 +1,12 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+
 export default function Home() {
   return (
-<div className="bg-red-500">testing colors</div>
+<div className="bg-blue-900 w-screen h-screen flex items-center">
+  <div className="text-center w-full">
+  <button className="bg-white p-2 rounded-lg px-4">Login with Google</button>
+  </div>
+
+</div>
   )
 }
