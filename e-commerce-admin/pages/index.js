d87155f81685a0c7a871cec775e95415e4a2f1a0
oscarlos Gomez Rosario <oscarlosg14@hotmail.com>
2023-11-17 20:58:34 -0500

    added google auth and mongodb conneciton. i love this.

--- e-commerce-admin/pages/index.js
+++ e-commerce-admin/pages/index.js
@@ -1,12 +1,33 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
-  return (
-<div className="bg-blue-900 w-screen h-screen flex items-center">
-  <div className="text-center w-full">
-  <button className="bg-white p-2 rounded-lg px-4">Login with Google</button>
-  </div>
+  const { data: session } = useSession();
 
-</div>
-  )
+  if (session) {
+    return (
+      <>
+        <button
+          onClick={() => signOut()}
+          className="bg-white p-2 rounded-lg px-4"
+        >
+          sign out
+        </button>
+        <div className=""> We are logged in through google! yahoo!!!!</div>
+        Signed in as {session.user.name} <br />
+      </>
+    );
+  } else {
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button
+            onClick={() => signIn("google")}
+            className="bg-white p-2 rounded-lg px-4"
+          >
+            Login with Google
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
