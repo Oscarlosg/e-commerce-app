1500be7630ff0e1594aef191fd45d010e2788441
oscarlos Gomez Rosario <oscarlosg14@hotmail.com>
2023-11-20 19:31:17 -0500

    added functionality and handlesrs to new product form. working on models now.

--- e-commerce-admin/pages/products/new.js
+++ e-commerce-admin/pages/products/new.js
@@ -1,36 +1,45 @@
 import Layout from "@/components/Layout";
+import axios from "axios";
 import { useState } from "react";
 
 const NewProduct = () => {
   const [title, SetTitle] = useState("");
   const [description, SetDescription] = useState("");
-  const [price, SetPrice] = useState("");
-
+  const [price, SetPrice] = useState();
+  async function createProduct(e) {
+    e.preventDefault()
+    const data = { title, description, price };
+    await axios.post("api/products", data);
+  }
   return (
     <Layout>
-      <h1>New Product</h1>
-      <label>Product Name</label>
-      <input
-        type="text"
-        placeholder="product name"
-        value={title}
-        onChange={(e) => SetTitle(e.target.value)}
-      />
-      <label>Description</label>
-      <textarea
-        placeholder="description"
-        className="pb-[4em]"
-        value={description}
-        onChange={(e) => SetDescription(e.target.value)}
-      />
-      <label>Price (USD only)</label>
-      <input
-        type="number"
-        placeholder="price"
-        value={price}
-        onChange={(e) => SetPrice(e.target.value)}
-      />
-      <button className="btn-primary">Save</button>
+      <form action="" onSubmit={createProduct} className="">
+        <h1>New Product</h1>
+        <label>Product Name</label>
+        <input
+          type="text"
+          placeholder="product name"
+          value={title}
+          onChange={(e) => SetTitle(e.target.value)}
+        />
+        <label>Description</label>
+        <textarea
+          placeholder="description"
+          className="pb-[4em]"
+          value={description}
+          onChange={(e) => SetDescription(e.target.value)}
+        />
+        <label>Price (USD only)</label>
+        <input
+          type="number"
+          placeholder="price"
+          value={price}
+          onChange={(e) => SetPrice(e.target.value)}
+        />
+        <button className="btn-primary" type="submit">
+          Save
+        </button>
+      </form>
     </Layout>
   );
 };
